41632cec9909928796cd6fdeb802d1b64a5d28d8
Yavor Papazov <y_papaz@yahoo.com>
2023-02-13 10:08:56 -0500

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { useState, useEffect } from "react"
 
 function App() {
-  let [count, setCount] = useState(0)
-  
+  let [data, setData] = useState('')
+  async function getData() {
+    let url = 'https://swapi.dev/api/people/1'
+    let result = await fetch(url)
+    console.log(result)
+    let myData = await result.json()
+    setData(myData)
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+  console.log(data)
   return (
     <div>
-      <h3>{count}</h3>
+      <h3>Hello, Friends!</h3>
+      <h3>{data.name}</h3>
     </div>
   )
 }
