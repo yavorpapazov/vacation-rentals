3284232752b5b6c963d58b8af311aaa77c9b31f8
Yavor Papazov <y_papaz@yahoo.com>
2023-02-27 17:05:16 -0500

    commit

---
+++ src/routes/ErrorPage.js
@@ -0,0 +1,7 @@
+function ErrorPage() {
+  return (
+    <h3>Error Page</h3>
+  )
+}
+
+export default ErrorPage
---
+++ src/routes/Home.js
@@ -0,0 +1,97 @@
+import classes from "./Home.module.css"
+import { useState, useEffect } from "react"
+import { db, storage } from "../firebase/firebase-config"
+import { collection, onSnapshot, doc, getDoc, setDoc, deleteDoc } from "firebase/firestore"
+import { ref, deleteObject } from "firebase/storage"
+import VacationRental from "../components/VacationRental"
+import ShoppingCart from "../components/ShoppingCart"
+import Form from "../components/Form"
+import Button from "../ui/Button"
+
+function Home() {
+  let [cart, setCart] = useState([])
+  let [bnbs, setBnbs] = useState([])
+  let [isShoppingCartDisplayed, setIsShoppingCartDisplayed] = useState(false)
+  async function handleAddToCart(bnbId) {
+    let addDocRef = doc(db, "bnbs", bnbId)
+    let docSnap = await getDoc(addDocRef)
+    let addCartDocRef = doc(db, "cart", bnbId)
+    let addCartDocSnap = await getDoc(addCartDocRef)
+    if(addCartDocSnap.exists()) {
+      alert('The item is already in the cart.')
+      return
+    }
+    await setDoc(doc(db, "cart", bnbId), docSnap.data())
+  }
+  async function handleRemoveFromCart(bnbId) {
+    let deleteCartDocRef = doc(db, "cart", bnbId)
+    await deleteDoc(deleteCartDocRef)
+  }
+  function handleCloseCart() {
+    setIsShoppingCartDisplayed(false)
+  }
+  async function handleDelete(docId) {
+    let addCartDocRef = doc(db, "cart", docId)
+    let addCartDocSnap = await getDoc(addCartDocRef)
+    if(addCartDocSnap.exists()) {
+      alert('Please remove item from shopping cart.')
+      return
+    }
+    let deleteDocRef = doc(db, "bnbs", docId)
+    let docSnap = await getDoc(deleteDocRef)
+    await deleteDoc(deleteDocRef)
+    let deleteImageRef = ref(storage, docSnap.data().fullPath)
+    deleteObject(deleteImageRef)
+  }
+  useEffect(() => {
+    let bnbsCollectionRef = collection(db, "bnbs")
+    let getBnbs = async () => {
+      try {
+        onSnapshot(bnbsCollectionRef, snapshot => {
+          let result = snapshot.docs.map(doc => ({...doc.data(), id: doc.id}))
+          setBnbs(result)
+        })
+      } catch(err) {
+        console.error(err)
+      }
+    }
+    getBnbs()
+  }, [])
+  useEffect(() => {
+    let cartCollectionRef = collection(db, "cart")
+    let getCart = async () => {
+      onSnapshot(cartCollectionRef, snapshot => {
+        let result = snapshot.docs.map(doc => ({...doc.data(), id: doc.id}))
+        setCart(result)
+      })
+    }
+    getCart()
+  }, [])
+  let resultVacationRental = bnbs.map(item => <VacationRental 
+    key={item.id} 
+    bnb={item} 
+    manageCart={handleAddToCart} 
+    deleteBnb={handleDelete} 
+    action="Add to Cart" 
+    showDelete={true}
+  />)
+  return (
+    <div className={classes.container}>
+      {isShoppingCartDisplayed && <div className={classes.backdrop} />}
+      <Form />
+      <div>
+        <h3 className={classes["shopping-cart-h3"]}>Shopping cart items: {cart.length}</h3>
+        <Button addClass="button" onClick={() => setIsShoppingCartDisplayed(true)}>Shopping Cart</Button>
+      </div>
+      <div className={classes["grid-container"]}>
+        {resultVacationRental}
+      </div>
+      {isShoppingCartDisplayed && 
+      <div className={classes.modal}>
+        <ShoppingCart bnbCart={cart} manageCart={handleRemoveFromCart} closeCart={handleCloseCart} />
+      </div>}
+    </div>
+  );
+}
+
+export default Home
