85ccfc63589d77134553a64caeeaf2608bbd4132
Yavor Papazov <y_papaz@yahoo.com>
2023-03-01 13:26:40 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/bnbs/:id" element={<SingleBnb />} />
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<ErrorPage />} />
       </Route>
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   )
 }
