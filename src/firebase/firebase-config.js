fd313d6e1b214753e97aa3688d21f4f3bc7d307b
Yavor Papazov <y_papaz@yahoo.com>
2023-03-04 13:57:45 -0500

    commit

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-import { getStorage } from "firebase/storage"
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBhdnnMx3LV6M6VYmVViOplfYGHnHP0k6E",
@@ -13,5 +14,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const storage = getStorage(app);
 export const db = getFirestore(app);
--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,17 +1,61 @@
 import classes from "./Login.module.css"
+import { useState } from "react"
+import { auth, googleProvider } from "../firebase/firebase-config"
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth"
 import { useNavigate } from "react-router-dom"
 import Button from "../ui/Button"
 
 function Login() {
+  let [email, setEmail] = useState('')
+  let [password, setPassword] = useState('')
   let navigate = useNavigate()
-  function handleLogin() {
-    alert('You have successfully logged in.')
-    navigate('/')
+  function handleEmail(e) {
+    setEmail(e.target.value)
   }
+  function handlePassword(e) {
+    setPassword(e.target.value)
+  }
+  async function handleSignup(e) {
+    try {
+      e.preventDefault()
+      await createUserWithEmailAndPassword(auth, email, password)
+      setEmail('')
+      setPassword('')
+    } catch(err) {
+      console.error(err)
+    }
+    //navigate('/')
+  }
+  async function handleSigninWithGoogle() {
+    try {
+      await signInWithPopup(auth, googleProvider)
+    } catch(err) {
+      console.error(err)
+    }
+  }
+  async function handleLogout() {
+    try {
+      await signOut(auth)
+    } catch(err) {
+      console.error(err)
+    }
+  }
+  console.log(auth?.currentUser?.email)
   return (
     <div className={classes.login}>
-      <h3>Login Page</h3>
-      <Button addClass="button" onClick={handleLogin}>Log In</Button>
+      <div className={classes["form-item"]}>
+        <label htmlFor="email">Email:</label>
+        <br />
+        <input id="email" type="email" value={email} onChange={handleEmail} required></input>
+      </div>
+      <div className={classes["form-item"]}>
+        <label htmlFor="password">Password:</label>
+        <br />
+        <input id="password" type="password" value={password} onChange={handlePassword} required></input>
+      </div>
+      <Button addClass="button" onClick={handleSignup}>Sign up</Button>
+      <Button addClass="button" onClick={handleSigninWithGoogle}>Sign in with Google</Button>
+      <Button addClass="button" onClick={handleLogout}>Log out</Button>
     </div>
   )
 }
